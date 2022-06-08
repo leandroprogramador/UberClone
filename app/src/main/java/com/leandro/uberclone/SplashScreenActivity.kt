cff3d4d511202fd6cf6f7954bc519459de76f3b3
Leandro Araujo <lean.ib@hotmail.com>
2022-06-07 21:17:10 -0300

    - Homepage with map and location - Register online user

--- app/src/main/java/com/leandro/uberclone/SplashScreenActivity.kt
+++ app/src/main/java/com/leandro/uberclone/SplashScreenActivity.kt
@@ -1,15 +1,24 @@
 package com.leandro.uberclone
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.auth.AuthMethodPickerLayout
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import com.leandro.uberclone.model.DriverInfoModel
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Completable
 import java.util.concurrent.TimeUnit
@@ -23,6 +32,9 @@ class SplashScreenActivity : AppCompatActivity() {
     private lateinit var providers: List<AuthUI.IdpConfig>
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var listener: FirebaseAuth.AuthStateListener
+    lateinit var database : FirebaseDatabase
+    lateinit var driverInfoRef : DatabaseReference
+    lateinit var progressBar : ProgressBar
 
 
     private fun delaySplashScreen() {
@@ -36,11 +48,16 @@ class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        progressBar = findViewById(R.id.progress_bar)
         init()
 
     }
 
     private fun init() {
+        database = FirebaseDatabase.getInstance()
+        driverInfoRef = database.getReference(Common.DRIVER_INFO_REFERENCE)
+
+
         providers = listOf(
             AuthUI.IdpConfig.PhoneBuilder().build(),
             AuthUI.IdpConfig.GoogleBuilder().build()
@@ -50,11 +67,85 @@ class SplashScreenActivity : AppCompatActivity() {
         listener = FirebaseAuth.AuthStateListener { mFirebaseAuth ->
             val user = mFirebaseAuth.currentUser
             user?.let {
-                Toast.makeText(this@SplashScreenActivity, "Welcome: " + user.uid, Toast.LENGTH_SHORT).show()
+                checkUserFromFirebase()
             } ?: run {
                 showLoginLayout()
             }
         }
+    }
+
+    private fun checkUserFromFirebase() {
+        driverInfoRef.child(FirebaseAuth.getInstance().currentUser!!.uid)
+            .addListenerForSingleValueEvent(object : ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if(snapshot.exists()){
+                        val model = snapshot.getValue(DriverInfoModel::class.java)
+                        goToHomeActivity(model)
+                    } else {
+                        showRegisterLayout()
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Toast.makeText(this@SplashScreenActivity, error.message, Toast.LENGTH_SHORT).show()
+                }
+
+            })
+    }
+
+    private fun goToHomeActivity(model: DriverInfoModel?) {
+        Common.currentUser = model
+        startActivity(Intent(this, DriverHomeActivity::class.java))
+        finish()
+    }
+
+    private fun showRegisterLayout() {
+
+        val itemView = LayoutInflater.from(this).inflate(R.layout.layout_register, null)
+        val edtFirstName = itemView.findViewById<EditText>(R.id.edt_first_name)
+        val edtLastName = itemView.findViewById<EditText>(R.id.edt_last_name)
+        val edtPhone = itemView.findViewById<EditText>(R.id.edt_phone)
+        val btnContinue = itemView.findViewById<Button>(R.id.btn_continue)
+
+        val phoneNumber = FirebaseAuth.getInstance().currentUser!!.phoneNumber
+        if(phoneNumber != null && !TextUtils.isDigitsOnly(phoneNumber)) edtPhone.setText(phoneNumber)
+
+        val alertDialog = AlertDialog.Builder(this, R.style.DialogTheme).setView(itemView).create()
+
+        btnContinue.setOnClickListener {
+            if(TextUtils.isDigitsOnly(edtFirstName.text.toString())){
+                Toast.makeText(this@SplashScreenActivity, "Please enter a valid first name!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            } else if(TextUtils.isDigitsOnly(edtLastName.text.toString())){
+                Toast.makeText(this@SplashScreenActivity, "Please enter a valid last name!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            } else if(TextUtils.isDigitsOnly(edtPhone.text.toString())){
+                Toast.makeText(this@SplashScreenActivity, "Please enter a valid phone number!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            } else{
+                val model = DriverInfoModel().also {
+                    it.firstName = edtFirstName.text.toString()
+                    it.lastName = edtLastName.text.toString()
+                    it.phoneNumber = edtPhone.text.toString()
+                }
+                driverInfoRef.child(FirebaseAuth.getInstance().currentUser!!.uid)
+                    .setValue(model)
+                    .addOnFailureListener {
+                        Toast.makeText(this@SplashScreenActivity, it.message, Toast.LENGTH_SHORT).show()
+                        alertDialog.dismiss()
+                        progressBar.visibility = View.GONE
+                    }
+                    .addOnSuccessListener {
+                        Toast.makeText(this@SplashScreenActivity, "Register successfully!", Toast.LENGTH_SHORT).show()
+                        alertDialog.dismiss()
+                        goToHomeActivity(model)
+                        progressBar.visibility = View.GONE
+                    }
+
+            }
+        }
+        alertDialog.show()
+
     }
 
     private fun showLoginLayout(){
@@ -91,5 +182,12 @@ class SplashScreenActivity : AppCompatActivity() {
     override fun onStop() {
         if (firebaseAuth != null && listener != null) firebaseAuth.removeAuthStateListener(listener)
         super.onStop()
+    }
+
+    object Common{
+        var currentUser: DriverInfoModel? = null
+        val DRIVER_INFO_REFERENCE: String = "DriverInfo"
+        val DRIVERS_LOCATION_REFERENCE: String = "DriversLocation"
+
     }
 }
---
+++ app/src/main/java/com/leandro/uberclone/ui/home/HomeFragment.kt
@@ -0,0 +1,234 @@
+package com.leandro.uberclone.ui.home
+
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.content.res.Resources
+import android.os.Bundle
+import android.os.Looper
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RelativeLayout
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.firebase.geofire.GeoFire
+import com.firebase.geofire.GeoLocation
+import com.google.android.gms.common.internal.service.Common
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import com.leandro.uberclone.R
+import com.leandro.uberclone.SplashScreenActivity.Common.DRIVERS_LOCATION_REFERENCE
+import com.leandro.uberclone.SplashScreenActivity.Common.DRIVER_INFO_REFERENCE
+import com.leandro.uberclone.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment(), OnMapReadyCallback {
+
+    private val REQUEST_LOCATION: Int = 100
+    private var _binding: FragmentHomeBinding? = null
+    private lateinit var mMap: GoogleMap
+    private lateinit var mapFragment: SupportMapFragment
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCall: LocationCallback
+    private var fusedLocationProviderClient: FusedLocationProviderClient? = null
+    private val binding get() = _binding!!
+    private lateinit var onlineRef : DatabaseReference
+    private lateinit var currentUserRef : DatabaseReference
+    private lateinit var driversLocationRef : DatabaseReference
+    private lateinit var geoFire : GeoFire
+    private val onlineValueEventListener = object : ValueEventListener{
+        override fun onDataChange(snapshot: DataSnapshot) {
+            if(snapshot.exists()){
+                currentUserRef.onDisconnect().removeValue()
+            }
+        }
+
+        override fun onCancelled(error: DatabaseError) {
+            Snackbar.make(mapFragment.requireView(), error.message, Snackbar.LENGTH_LONG).show()
+        }
+
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        init()
+        mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        return root
+    }
+
+    @SuppressLint("MissingPermission")
+    fun init() {
+        onlineRef = FirebaseDatabase.getInstance().getReference().child(".info/connected")
+        driversLocationRef = FirebaseDatabase.getInstance().getReference(DRIVERS_LOCATION_REFERENCE)
+        currentUserRef = FirebaseDatabase.getInstance().getReference(DRIVERS_LOCATION_REFERENCE).child(
+            FirebaseAuth.getInstance().currentUser!!.uid
+        )
+        geoFire = GeoFire(driversLocationRef)
+        registerOnlineSystem()
+
+        locationRequest = LocationRequest.create()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.fastestInterval = 3000
+        locationRequest.interval = 5000
+        locationRequest.smallestDisplacement = 10f
+
+
+        locationCall = object : LocationCallback() {
+            @SuppressLint("MissingPermission")
+            override fun onLocationResult(locationResult: LocationResult) {
+                super.onLocationResult(locationResult)
+
+                if (!locationResult.locations.isNullOrEmpty()) {
+                    val latLng = LatLng(
+                        locationResult.lastLocation.latitude,
+                        locationResult.lastLocation.longitude
+                    )
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 18f))
+                    geoFire.setLocation(
+                        FirebaseAuth.getInstance().currentUser!!.uid,
+                        GeoLocation(locationResult.lastLocation.latitude, locationResult.lastLocation.longitude)
+                    ){key : String?, error: DatabaseError ? ->
+                        error?.let {
+                            Snackbar.make(mapFragment.requireView(), error.message, Snackbar.LENGTH_LONG).show()
+                        } ?: run {
+                            Snackbar.make(mapFragment.requireView(), "You 're online!", Snackbar.LENGTH_LONG).show()
+                        }
+                    }
+
+                }
+            }
+        }
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext())
+        fusedLocationProviderClient!!.requestLocationUpdates(locationRequest, locationCall, Looper.myLooper()!!)
+        getLocation()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onDestroy() {
+        fusedLocationProviderClient!!.removeLocationUpdates(locationCall)
+        geoFire.removeLocation(FirebaseAuth.getInstance().currentUser!!.uid)
+        onlineRef.removeEventListener(onlineValueEventListener)
+        super.onDestroy()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        registerOnlineSystem()
+    }
+
+    private fun registerOnlineSystem() {
+        onlineRef.addValueEventListener(onlineValueEventListener)
+
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_DENIED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_DENIED
+        ) {
+
+            configureLocationButton()
+
+        } else {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.ACCESS_FINE_LOCATION, ),
+                REQUEST_LOCATION
+            )
+
+        }
+
+        try {
+            val success = googleMap.setMapStyle(
+                MapStyleOptions.loadRawResourceStyle(
+                    requireContext(),
+                    R.raw.uber_maps_style
+                )
+            )
+            if (!success) {
+                Log.e("UBER ERROR", "Style map parsing error")
+            }
+        } catch (ex: Resources.NotFoundException) {
+            Log.e("UBER ERROR", ex.message!!)
+        }
+
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun configureLocationButton() {
+        mMap.isMyLocationEnabled = true
+        mMap.uiSettings.isMyLocationButtonEnabled = true
+        mMap.setOnMyLocationButtonClickListener {
+            getLocation()
+
+            return@setOnMyLocationButtonClickListener true
+        }
+
+
+        val locationButtom = (mapFragment.requireView()
+            .findViewById<View>("1".toInt())
+            .parent as View).findViewById<View>("2".toInt())
+        val params = locationButtom.layoutParams as RelativeLayout.LayoutParams
+        params.addRule(RelativeLayout.ALIGN_TOP, 0)
+        params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE)
+        params.bottomMargin = 50
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLocation() {
+        fusedLocationProviderClient!!.lastLocation.addOnFailureListener { e ->
+            Toast.makeText(requireContext(), e.message, Toast.LENGTH_SHORT).show()
+        }.addOnSuccessListener { location ->
+            val latLng = LatLng(location.latitude, location.longitude)
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 18f))
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (grantResults.size > 0 && requestCode == REQUEST_LOCATION) {
+            if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                Toast.makeText(requireContext(), "Permissao DENIED", Toast.LENGTH_SHORT).show()
+                requireActivity().finish()
+            } else{
+                init()
+                configureLocationButton()
+            }
+        }
+    }
+
+}
